b3e005c84daecf848b5dfda0056ef32802c47aa2
Krishna Prasad Y V S Purama <81025229+krishnaprasad12@users.noreply.github.com>
2023-10-11 23:04:22 +0530

    Project resumed and started working on it

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -17,7 +17,10 @@ export default function RootLayout({
   return (
     <ClerkProvider>
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        
+        {children}
+      </body>
     </html>
     </ClerkProvider>
   )
